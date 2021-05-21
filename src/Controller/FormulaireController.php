d2ee8e0e4c8e31237d0fc10ec4aaab37e9903c25
Nicolas VAREILLE <nicolasvareille.gdl@gmail.com>
2021-05-21 16:03:06 +0200

    Ajout des datas dans la BDD

--- src/Controller/FormulaireController.php
+++ src/Controller/FormulaireController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Pokemon;
 use App\Form\PokemonFormType;
+use App\Repository\PokemonRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,6 +12,22 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class FormulaireController extends AbstractController
 {
+    /**
+     * @Route ("/pokemon")
+     */
+    public function PokemonListe()
+    {
+        $pokemons = $this->getDoctrine()
+            ->getRepository(Pokemon::class)
+            ->findAll();
+
+        return ($this->render('formulaire/pokemon.html.twig',
+        [
+            'pokemons' => $pokemons
+        ]));
+    }
+
+
     /**
      * @Route("/formulaire", name="formulairePost", methods={"POST"})
      */
@@ -20,8 +38,17 @@ class FormulaireController extends AbstractController
 
         $data = $form->getData();
 
-        var_dump($data);
-        die();
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $pokemon = new Pokemon();
+        $pokemon->setNom($data["Nom"]);
+        $pokemon->setDescription($data["Description"]);
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($pokemon);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
 
         return $this->render('formulaire/index.html.twig',
         [
--- src/Form/PokemonFormType.php
+++ src/Form/PokemonFormType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,6 +17,7 @@ class PokemonFormType extends AbstractType
         $builder
             ->add('Id', IntegerType::class)
             ->add('Nom', TextType::class)
+            ->add("Description", TextareaType::class)
             ->add("Enregistrer", SubmitType::class)
         ;
     }
